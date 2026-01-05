faf397f714bdf38804aa52c5ef081ffbc246bf44
Justine <justine.faure98@outlook.com>
2026-01-05 16:05:08 +0100

    Fix Projects page & add config

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -25,6 +25,7 @@ const projects: ProjectData[] = [
     description: "Création d'un moteur de jeu from scratch en JavaScript pur (Vanilla). Gestion de la physique, des collisions et du rendu graphique sans framework.",
     stack: ['JavaScript (ES6+)', 'HTML5 Canvas', 'CSS3', 'OOP Pattern'],
     repoLink: 'https://github.com/Ailura4020/make-your-game',
+    image: '/projects/makeyourgame.png'
   },
   {
     id: 'p4', codename: 'REAL-TIME-COMMS', title: 'REAL TIME FORUM', status: 'TERMINÉ', type: 'ECOLE',
@@ -32,7 +33,6 @@ const projects: ProjectData[] = [
     stack: ['Go (Golang)', 'SQLite', 'Docker', 'WebSockets', 'JS'],
     repoLink: 'https://github.com/Ailura4020/real-time-forum.git',
     image: '/projects/real-time-forum.png' 
-
   },
   {
     id: 'p5', codename: 'TCP-NET-PROTOCOL', title: 'NET-CAT', status: 'TERMINÉ', type: 'ECOLE',
