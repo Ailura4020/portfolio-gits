386aeb80f218b68b3ed793edbf31e4374d10e16b
Justine <justine.faure98@outlook.com>
2026-05-07 15:09:51 +0200

    ajout projet du start plateforme dans section projets

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -8,9 +8,11 @@ export interface ProjectData {
   status: string;
   type: string;
   description: string;
+  fullDesc?: React.ReactNode; 
   stack: string[];
-  repoLink: string;
+  repoLink?: string;          
   image?: string;
+  gallery?: string[];        
 }
 
 // CORRECTION ICI : On garde la définition <{... isMobile?: boolean }> mais on retire 'isMobile' des accolades ({...})
